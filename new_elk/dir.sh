41e4e2af2a26eadb31fc483b8b2d4bfe7edceeb4
KDimash <69385214+KDimash@users.noreply.github.com>
2022-08-18 11:36:49 +0600

    Add files via upload

---
+++ new_elk/dir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir /data_elk
+mkdir /data_elk/data01
+mkdir /data_elk/data02
+mkdir /data_elk/data03
+mkdir /data_elk/shared
+mkdir /data_elk/shared/es_certs
+
+mkdir /data_elk/logstash
+mkdir /data_elk/logstash/pipelines
+
+mkdir /data_elk/thehive
+mkdir /data_elk/thehive/db
+mkdir /data_elk/thehive/data
+mkdir /data_elk/thehive/index
